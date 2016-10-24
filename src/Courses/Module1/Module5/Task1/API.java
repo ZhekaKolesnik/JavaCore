8a1a1e8b2764f220fa89638c33811df9f4f8ca0a
ZhekaKolesnik <joopster15@gmail.com>
2016-10-24 12:30:37 +0300

    Homework

--- src/Courses/Module1/Module5/Task1/API.java
+++ src/Courses/Module1/Module5/Task1/API.java
@@ -1,8 +1,18 @@
 package Courses.Module1.Module5.Task1;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by Жека on 24.10.2016.
  */
 public interface API {
   Room[]findRoom(int price ,int persons,String city,String hotel);
+    int ONE_DAY_SHIFT = 1;
+    static Date getDate(int amount){
+        Calendar call= new GregorianCalendar();
+        call.add(ONE_DAY_SHIFT,amount);
+        return call.getTime();
+    }
 }
---
+++ src/Courses/Module1/Module5/Task1/GoogleAPI.java
@@ -0,0 +1,38 @@
+package Courses.Module1.Module5.Task1;
+
+import java.awt.*;
+import java.awt.List;
+import java.util.*;
+
+/**
+ * Created by Жека on 24.10.2016.
+ */
+public class GoogleAPI implements API{
+    private Room[]rooms=new Room[5];
+    public GoogleAPI(){
+        Room room1 = new Room(12,1200,1,API.getDate(ONE_DAY_SHIFT),"Ukraine","Kyiv");
+        Room room2 = new Room(11,1000,1,API.getDate(ONE_DAY_SHIFT),"Lybid","Kyiv");
+        Room room3 = new Room(10,2000,1,API.getDate(ONE_DAY_SHIFT),"Merco","Kyiv");
+        Room room4= new Room(240,4500,1,API.getDate(ONE_DAY_SHIFT),"lisdo","Kharkiv");
+        Room room5 = new Room(345,600,1,API.getDate(ONE_DAY_SHIFT),"Ukraine","Kyiv");
+    }
+
+
+    @Override
+    public Room[] findRoom(int price, int persons, String city, String hotel) {
+        Room toSearch = new Room(-1, price, persons, null, city, hotel);
+        Room[] arrayOfRooms = new Room[1000];
+        java.util.List<Room>result=new ArrayList<>();
+
+
+        if (toSearch.getPrice()==0 && toSearch.getPersons() == 0 && !"null".equals(toSearch.getHotelName()) && !"null".equals(toSearch.getCityName())){
+            return rooms;
+        }
+        for (Room room : rooms) {
+            if (room.equals(toSearch) ) {
+                result.add(room);
+            }
+        }
+        return result.toArray(new Room[result.size()]);
+    }
+}
