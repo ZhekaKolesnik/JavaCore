83f93023dbe630b816c243f819fc151ca41f1fa8
ZhekaKolesnik <joopster15@gmail.com>
2016-10-26 14:28:21 +0300

    Homework

---
+++ src/Coolledge/Tech/Test.java
@@ -0,0 +1,357 @@
+package Coolledge.Tech;
+
+/**
+ * Created by Жека on 26.10.2016.
+ */
+public class Test {
+
+        public static void main(String[] args) {
+            Scanner otv = new Scanner(System.in);
+            int bal = 0;
+            System.out.println("1. Irene ___ a good driver.");
+            System.out.println("a)am");
+            System.out.println("b)are");
+            System.out.println("c)is");
+            System.out.println("d)she");
+            String o1 = sc.nextLine();
+            switch (o1) {
+                case "a" :
+                    break;
+                case "b":
+                    break;
+                case "c":
+                    ocinka++;
+                    break;
+                case "d":
+                    break;
+            }
+            System.out.println("2. Women ____ flowers.");
+            System.out.println("a)like");
+            System.out.println("b)likes");
+            System.out.println("c)liken");
+            System.out.println("d)liking");
+            String o2 = otv.nextLine();
+            switch (o2) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("3. Look! The boys ___ running!");
+            System.out.println("a)is");
+            System.out.println("b)are");
+            System.out.println("c)am");
+            System.out.println("d)they");
+            String o3 = otv.nextLine();
+            switch (o3) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("4._____the weather often change in Britain?");
+            System.out.println("a)Is");
+            System.out.println("b)Are");
+            System.out.println("c)Do");
+            System.out.println("d)Does");
+            String o4 = sc.nextLine();
+            switch (o4) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    ocinka++;
+                    break;
+            }
+            System.out.println("5.Kelly and Andrew ___ next door to us.");
+            System.out.println("a)are living");
+            System.out.println("b)live");
+            System.out.println("c)lives");
+            System.out.println("d)is living");
+            String o5 = otv.nextLine();
+            switch (o5) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("6. Granny ___ better now, thank you.");
+            System.out.println("a)is feeling");
+            System.out.println("b)feels");
+            System.out.println("c)feel");
+            System.out.println("d)feeling");
+            String o6 = otv.nextLine();
+            switch (o6) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    ocinka++;
+                    break;
+            }
+            System.out.println("7. ___ you got any good magazines?");
+            System.out.println("a)Do");
+            System.out.println("b)Does");
+            System.out.println("c)Has");
+            System.out.println("d)Have");
+            String o7 = otv.nextLine();
+            switch (o7) {
+                case ("a"):
+                    ocinka++;
+                    break;
+                case ("b"):
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("8. My parents leave home for work ___ eight o’clock");
+            System.out.println("a)at");
+            System.out.println("b)in");
+            System.out.println("c)when");
+            System.out.println("d)on");
+            String o8 = otv.nextLine();
+            switch (o8) {
+                case ("a"):
+                    ocinka++;
+                    break;
+                case ("b"):
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("9. We ___ buy food in this supermarket. It’s very expensive here.");
+            System.out.println("a)not");
+            System.out.println("b)don't");
+            System.out.println("c)aren't");
+            System.out.println("d)doesn't");
+            String o9 = otv.nextLine();
+            switch (o9) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("10. Your teeth ___ bad. Consult the dentist, please.");
+            System.out.println("a)are");
+            System.out.println("b)is");
+            System.out.println("c)am");
+            System.out.println("d)do");
+            String o10 = otv.nextLine();
+            switch (o10) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("11. ___ you ready for the lesson?");
+            System.out.println("a)Is");
+            System.out.println("b)Are");
+            System.out.println("c)Do");
+            System.out.println("d)Does");
+            String o11 = otv.nextLine();
+            switch (o11) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("12. I like English. I ___ it with pleasure to read and speak the language well.");
+            System.out.println("a)learn");
+            System.out.println("b)learning");
+            System.out.println("c)teaching");
+            System.out.println("d)teach");
+            String o12 = otv.nextLine();
+            switch (o12) {
+                case ("a"):
+                    ocinka++;
+                    break;
+                case ("b"):
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("13. She ___ breakfast at 7 o’clock.");
+            System.out.println("a)has got");
+            System.out.println("b)have got");
+            System.out.println("c)have");
+            System.out.println("d)has");
+            System.out.println("e)haves");
+            String o13 = otv.nextLine();
+            switch (o13) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    ocinka++;
+                    break;
+                case ("e"):
+                    break;
+            }
+            System.out.println("14. Who ___ you up every day?");
+            System.out.println("a)is ringing");
+            System.out.println("b)rings");
+            System.out.println("c)ring");
+            System.out.println("d)are ringing");
+            String o14 = otv.nextLine();
+            switch (o14) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("15. ___ stop! Read more.");
+            System.out.println("a)Not");
+            System.out.println("b)Don't");
+            System.out.println("c)No");
+            System.out.println("d)Doesn't");
+            String o15 = otv.nextLine();
+            switch (o15) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("16. ___ ready for the lesson, please!");
+            System.out.println("a)Be");
+            System.out.println("b)Are");
+            System.out.println("c)Do");
+            System.out.println("d)Does");
+            String o16 = otv.nextLine();
+            switch (o16) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("17. Who ___ sitting next to you? – My parents are.");
+            System.out.println("a)are");
+            System.out.println("b)-");
+            System.out.println("c)do");
+            System.out.println("d)is");
+            String o17 = otv.nextLine();
+            switch (o17) {
+                case ("a"):
+                    ocinka++;
+                    break;
+                case ("b"):
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("18. What do you like to have for breakfast?");
+            System.out.println("a)I like to have a cup of tea with a toast");
+            System.out.println("b)Me like to have a cup of tea with a toast.");
+            System.out.println("c)Me likes to have a cup of tea with a toast.");
+            System.out.println("d)I likes to have to have a cup of tea with a toast.");
+            String o18 = otv.nextLine();
+            switch (o18) {
+                case ("a"):
+                    ocinka++;
+                    break;
+                case ("b"):
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("19. There ___ no books on the shelf.");
+            System.out.println("a)-");
+            System.out.println("b)are");
+            System.out.println("c)is");
+            System.out.println("d)don't");
+            String o19 = otv.nextLine();
+            switch (o19) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println("20.I go to the University ___ bus.");
+            System.out.println("a)in");
+            System.out.println("b)by");
+            System.out.println("c)on");
+            System.out.println("d)at");
+            String o20 = otv.nextLine();
+            switch (o20) {
+                case ("a"):
+                    break;
+                case ("b"):
+                    ocinka++;
+                    break;
+                case ("c"):
+                    break;
+                case ("d"):
+                    break;
+            }
+            System.out.println(ocinka);
+        }
+    }
+
